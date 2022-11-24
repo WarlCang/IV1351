f3d7fe8727b526b8d408c3bf032fd3e74262f8b8
WarlCang <carlwang1124@outlook.com>
2022-11-24 13:52:11 +0100

    Create data_input.sql

---
+++ data_input.sql
@@ -0,0 +1,487 @@
+INSERT INTO student (id,name,person_number,zip,street,city,skill_type,skill_level,phone,email)
+VALUES
+  (1,'Jolene','41168170K','85-26','8455 Quisque Street','Oteppe','violin','beginner','0845 46 46','neque@icloud.com'),
+  (2,'Gisela','34464197','26277','Ap #423-4118 Enim. Road','Balclutha','sitar','beginner','0800 185155','lorem@icloud.ca'),
+  (3,'Edward','105124791','72781','568-5606 Phasellus Ave','Thủ Dầu Một','sitar','intermediate','0800 644 7119','ut.nulla@outlook.org'),
+  (4,'Samuel','83177756','76-033','P.O. Box 456, 7368 Non, St.','Hattem','harp','advanced','0339 980 1228','lorem.semper@yahoo.org'),
+  (5,'Nash','398741676','4141','Ap #420-4233 Auctor. Ave','Tabuk','piano','advanced','(0117) 727 6588','vitae.sodales@protonmail.edu'),
+  (6,'Madonna','82153810','47662-020','Ap #587-8201 Parturient St.','Vänersborg','violin','advanced','0800 593761','diam.eu@icloud.ca'),
+  (7,'Amos','37184905K','27726','Ap #168-1971 Tellus Rd.','Logroño','sitar','beginner','0845 46 49','est.mauris@aol.edu'),
+  (8,'Marshall','159977358','449729','7229 Auctor, Av.','Kirkwall','piano','beginner','055 1330 1388','et.magnis@protonmail.couk'),
+  (9,'Ezra','148077304','670741','2311 Molestie Ave','Yurimaguas','veena','advanced','055 3884 4472','et@protonmail.edu'),
+  (10,'Garrett','361132629','643322','316-7121 A St.','Llaillay','harp','intermediate','056 4536 6247','lobortis@outlook.com');
+INSERT INTO student (id,name,person_number,zip,street,city,skill_type,skill_level,phone,email)
+VALUES
+  (11,'Seth','3734527K','898960','342-5650 Donec St.','Bacolod','mandolin','beginner','076 1464 7550','sollicitudin.adipiscing@icloud.ca'),
+  (12,'Nathaniel','394267759','302646','826-8402 Erat Avenue','Kediri','drums','advanced','070 6202 3356','ac.nulla@protonmail.edu'),
+  (13,'Judah','24329570K','39926','P.O. Box 680, 7135 Facilisi. Av.','Mesa','sitar','intermediate','0800 456492','amet.luctus@outlook.com'),
+  (14,'Meghan','95531903','4085','Ap #992-9567 Iaculis, Rd.','Oaxaca','sitar','beginner','07624 881523','ante.dictum@protonmail.edu'),
+  (15,'Charity','10643813','4440','463-3778 Eget, St.','Bạc Liêu','guitar','advanced','056 6520 3556','nisi@hotmail.org'),
+  (16,'Dawn','313865835','9253 JC','943-8962 Penatibus Street','Damoh','sax','intermediate','(0115) 154 4918','cursus.non@yahoo.edu'),
+  (17,'Blake','253005300','642826','P.O. Box 542, 1577 Ridiculus Av.','Novosibirsk','veena','beginner','07624 614634','cubilia@aol.edu'),
+  (18,'Beverly','4381793','2854','Ap #827-1835 Nec, Ave','Turrialba','guitar','advanced','056 6911 3640','faucibus.orci@outlook.edu'),
+  (19,'Cyrus','477491316','16585','Ap #217-518 Imperdiet Avenue','Oudergem','veena','intermediate','0917 397 6635','eu@yahoo.net'),
+  (20,'Cameran','188822509','25638','Ap #925-3257 A Road','Te Puke','sitar','advanced','(0115) 889 6875','aptent.taciti@icloud.net');
+INSERT INTO student (id,name,person_number,zip,street,city,skill_type,skill_level,phone,email)
+VALUES
+  (21,'Jordan','30265705K','14272','9055 Sem Rd.','Arequipa','mandolin','beginner','07624 753167','scelerisque.scelerisque@yahoo.net'),
+  (22,'Jocelyn','30895371','2301','478-5640 Sit Rd.','San Pietro Mussolino','drums','beginner','(01330) 71132','turpis@yahoo.com'),
+  (23,'Colton','42933384','285251','Ap #259-338 Et, Rd.','Dubbo','harp','intermediate','(024) 5261 8894','consectetuer@google.edu'),
+  (24,'Brenden','347228982','T7Y 3E7','911-7777 Nam Road','Winnipeg','harp','advanced','07624 616635','lorem.ipsum.sodales@outlook.net'),
+  (25,'Zephr','204832315','01233','277-1773 Nam Rd.','Ensenada','piano','beginner','0500 342521','lorem.sit@outlook.net');
+
+
+INSERT INTO instructor (id,name,person_number,can_teach_ensembles,zip,street,city,phone,email)
+VALUES
+  (1,'Galena','40703012','No','1175','P.O. Box 495, 5177 Tortor Rd.','Buren','(01455) 71058','ipsum@icloud.couk'),
+  (2,'Kelsey','229761404','Yes','21496','P.O. Box 785, 1003 Ipsum Rd.','Lapu-Lapu City','(016977) 1180','sed.nunc.est@aol.ca'),
+  (3,'Gregory','211521775','Yes','548151','Ap #124-1357 Neque. St.','Lillesand','(015677) 48751','purus.accumsan.interdum@outlook.edu'),
+  (4,'Guinevere','258853695','No','13058','Ap #395-3786 Integer St.','Middelburg','(024) 6593 4284','varius.orci.in@hotmail.com'),
+  (5,'Jolie','311842498','Yes','3819','150-9385 Pede Rd.','San Juan de Dios','(027) 1701 1127','in@google.org'),
+  (6,'Lucius','64519085','Yes','04-137','Ap #588-5073 Nunc Road','Gubbio','076 6841 0647','etiam@protonmail.org'),
+  (7,'Barrett','466662372','Yes','21773','4118 Quam, Av.','Trondheim','07339 668841','lorem.ipsum@google.com'),
+  (8,'Maggie','357291313','Yes','315612','Ap #600-1684 Dapibus Road','Flushing','070 8357 8227','at.iaculis@protonmail.couk'),
+  (9,'Evangeline','314842057','No','37781','Ap #508-9707 Euismod Rd.','Panguipulli','0951 514 2633','mattis.integer.eu@yahoo.couk'),
+  (10,'Joshua','229397192','No','18156','Ap #147-8628 Convallis Street','Pittsburgh','076 9394 4231','quis.massa.mauris@hotmail.com');
+
+INSERT INTO pricing_scheme (id,beginner_price,intermediate_price,individual_lesson_price,advanced_price,group_lesson_price,sibling_discount)
+VALUES
+  (1,825,510,1231,1082,1088,0.1);
+
+
+INSERT INTO lesson (id,level,intructor_id,price_scheme_id)
+VALUES
+  (1,'beginner',2,1),
+  (2,'advanced',5,1),
+  (3,'intermediate',5,1),
+  (4,'intermediate',4,1),
+  (5,'advanced',3,1),
+  (6,'advanced',4,1),
+  (7,'intermediate',10,1),
+  (8,'beginner',9,1),
+  (9,'beginner',4,1),
+  (10,'beginner',8,1);
+INSERT INTO lesson (id,level,intructor_id,price_scheme_id)
+VALUES
+  (11,'intermediate',10,1),
+  (12,'advanced',6,1),
+  (13,'beginner',1,1),
+  (14,'advanced',1,1),
+  (15,'intermediate',5,1),
+  (16,'intermediate',6,1),
+  (17,'advanced',4,1),
+  (18,'intermediate',4,1),
+  (19,'intermediate',8,1),
+  (20,'advanced',5,1);
+INSERT INTO lesson (id,level,intructor_id,price_scheme_id)
+VALUES
+  (21,'advanced',1,1),
+  (22,'advanced',6,1),
+  (23,'intermediate',9,1),
+  (24,'beginner',8,1),
+  (25,'advanced',10,1),
+  (26,'intermediate',7,1),
+  (27,'advanced',3,1),
+  (28,'intermediate',4,1),
+  (29,'advanced',6,1),
+  (30,'beginner',7,1);
+INSERT INTO lesson (id,level,intructor_id,price_scheme_id)
+VALUES
+  (31,'intermediate',10,1),
+  (32,'intermediate',3,1),
+  (33,'advanced',7,1),
+  (34,'advanced',7,1),
+  (35,'beginner',1,1),
+  (36,'advanced',8,1),
+  (37,'beginner',8,1),
+  (38,'advanced',4,1),
+  (39,'beginner',10,1),
+  (40,'beginner',7,1);
+
+INSERT INTO intruments (id,brand,quantity,type,price)
+VALUES
+  (1,'Sibelius',4,'sitar',286),
+  (2,'Finale',3,'guitar',340),
+  (3,'Chami',1,'piano',359),
+  (4,'Chami',4,'sax',494),
+  (5,'Finale',1,'veena',242),
+  (6,'Adobe',3,'piano',337),
+  (7,'Sibelius',5,'veena',275),
+  (8,'Apple',4,'guitar',357),
+  (9,'Microsoft',3,'veena',366),
+  (10,'Amazon',4,'sitar',301);
+INSERT INTO instruments (id,brand,quantity,type,price)
+VALUES
+  (11,'Adobe',2,'sax',357),
+  (12,'Cakewalk',3,'drums',491),
+  (13,'Amazon',4,'piano',429),
+  (14,'Google',2,'piano',499),
+  (15,'Chami',5,'veena',494),
+  (16,'Apple',5,'guitar',409),
+  (17,'Amazon',4,'sitar',306),
+  (18,'Cakewalk',1,'drums',280),
+  (19,'Amazon',5,'drums',384),
+  (20,'Sibelius',3,'sax',421);
+INSERT INTO instruments (id,brand,quantity,type,price)
+VALUES
+  (21,'Lavasoft',4,'sax',486),
+  (22,'Microsoft',2,'sitar',454),
+  (23,'Lavasoft',1,'drums',335),
+  (24,'Apple',2,'veena',453),
+  (25,'Amazon',5,'guitar',476),
+  (26,'Lavasoft',2,'veena',496),
+  (27,'Amazon',2,'sitar',279),
+  (28,'Borland',3,'piano',408),
+  (29,'Amazon',2,'sitar',268),
+  (30,'Lavasoft',4,'piano',437);
+INSERT INTO instruments (id,brand,quantity,type,price)
+VALUES
+  (31,'Microsoft',4,'sitar',204),
+  (32,'Adobe',3,'sitar',363),
+  (33,'Chami',4,'drums',239),
+  (34,'Chami',4,'veena',298),
+  (35,'Finale',5,'sax',221),
+  (36,'Microsoft',3,'piano',380),
+  (37,'Apple',4,'veena',422),
+  (38,'Lavasoft',4,'guitar',254),
+  (39,'Chami',3,'sitar',497),
+  (40,'Adobe',3,'guitar',314);
+
+
+INSERT INTO intrument_renting (student_id,intrument_id,time)
+VALUES
+  (4,17,'Sep 7, 2023'),
+  (11,27,'May 11, 2022'),
+  (6,15,'Aug 20, 2022'),
+  (22,6,'Nov 22, 2023'),
+  (6,18,'Mar 12, 2023'),
+  (18,20,'Oct 13, 2023'),
+  (17,11,'Jul 14, 2022'),
+  (4,13,'May 9, 2023'),
+  (5,32,'Feb 4, 2022'),
+  (23,5,'May 14, 2022');
+INSERT INTO intrument_renting (student_id,intrument_id,time)
+VALUES
+  (7,14,'May 12, 2022'),
+  (23,25,'Jan 23, 2023'),
+  (19,2,'Dec 2, 2022'),
+  (20,27,'Sep 21, 2022'),
+  (19,14,'Jul 25, 2023'),
+  (13,21,'Nov 13, 2023'),
+  (4,18,'Nov 4, 2022'),
+  (9,29,'Mar 5, 2023'),
+  (16,35,'Jan 18, 2022'),
+  (1,39,'Nov 10, 2022');
+
+INSERT INTO instrument_can_teach (id,name)
+VALUES
+  (1,'sax, violin'),
+  (2,'harp, violin, cello'),
+  (3,'harp, cello'),
+  (4,'guitar, veena, harp, sax, mandolin'),
+  (5,'sax, veena, violin, sitar'),
+  (6,'sax, violin, mandolin, cello'),
+  (7,'harp'),
+  (8,'harp, violin, guitar'),
+  (9,'violin, sax'),
+  (10,'guitar, sax, mandolin, drums, cello');
+
+INSERT INTO instrument_instructor_can_teach (instrument_can_teach_id,instructor_id)
+VALUES
+  (1,5),
+  (2,8),
+  (3,9),
+  (4,10),
+  (5,7),
+  (6,1),
+  (7,2),
+  (8,4),
+  (9,3),
+  (10,6);
+
+
+INSERT INTO is_avaliable_time (id,time)
+VALUES
+  (1,'2021-12-14 11:19:02'),
+  (2,'2021-12-21 04:06:23'),
+  (3,'2021-12-09 18:31:02'),
+  (4,'2021-11-29 00:09:15'),
+  (5,'2021-12-04 21:56:18'),
+  (6,'2021-12-08 21:40:51'),
+  (7,'2021-12-07 06:15:44'),
+  (8,'2021-12-22 17:36:48'),
+  (9,'2021-12-22 10:46:37'),
+  (10,'2021-11-25 18:32:23');
+INSERT INTO is_avaliable_time (id,time)
+VALUES
+  (11,'2021-12-12 16:57:49'),
+  (12,'2021-12-01 02:52:03'),
+  (13,'2021-11-30 05:57:01'),
+  (14,'2021-12-22 22:30:23'),
+  (15,'2021-12-16 23:55:34'),
+  (16,'2021-12-05 00:21:26'),
+  (17,'2021-12-04 21:28:38'),
+  (18,'2021-12-11 01:58:31'),
+  (19,'2021-12-14 05:22:27'),
+  (20,'2021-12-07 14:07:15');
+INSERT INTO is_avaliable_time (id,time)
+VALUES
+  (21,'2021-12-04 21:16:04'),
+  (22,'2021-12-02 18:21:05'),
+  (23,'2021-12-12 08:51:07'),
+  (24,'2021-12-13 03:46:06'),
+  (25,'2021-12-18 02:06:58'),
+  (26,'2021-12-02 14:47:25'),
+  (27,'2021-12-15 11:30:48'),
+  (28,'2021-12-10 12:48:19'),
+  (29,'2021-11-26 18:44:33'),
+  (30,'2021-11-25 00:18:58');
+INSERT INTO is_avaliable_time (id,time)
+VALUES
+  (31,'2021-12-02 14:15:28'),
+  (32,'2021-12-23 03:42:01'),
+  (33,'2021-12-02 01:40:05'),
+  (34,'2021-12-11 22:51:08'),
+  (35,'2021-12-09 10:37:06'),
+  (36,'2021-12-16 19:44:10'),
+  (37,'2021-12-03 17:38:07'),
+  (38,'2021-12-02 09:17:28'),
+  (39,'2021-11-30 06:30:55'),
+  (40,'2021-12-05 12:12:30');
+INSERT INTO is_avaliable_time (id,time)
+VALUES
+  (41,'2021-12-05 07:49:16'),
+  (42,'2021-12-15 21:48:11'),
+  (43,'2021-12-18 14:55:36'),
+  (44,'2021-12-22 17:23:47'),
+  (45,'2021-12-02 03:08:20'),
+  (46,'2021-12-02 13:52:57'),
+  (47,'2021-12-01 07:30:08'),
+  (48,'2021-12-11 14:32:05'),
+  (49,'2021-11-28 21:28:03'),
+  (50,'2021-12-01 14:09:37');
+
+INSERT INTO instructor_is_avaliable_time (instructor_id,is_avaliable_time_id)
+VALUES
+  ('5','46'),
+  ('7','24'),
+  ('5','4'),
+  ('9','14'),
+  ('4','5'),
+  ('2','45'),
+  ('6','36'),
+  ('4','41'),
+  ('5','5'),
+  ('2','26');
+INSERT INTO instructor_is_avaliable_time (instructor_id,is_avaliable_time_id)
+VALUES
+  ('1','38'),
+  ('8','50'),
+  ('6','8'),
+  ('1','35'),
+  ('10','17'),
+  ('4','33'),
+  ('7','20'),
+  ('3','28'),
+  ('4','39'),
+  ('7','36');
+INSERT INTO instructor_is_avaliable_time (instructor_id,is_avaliable_time_id)
+VALUES
+  ('6','25'),
+  ('9','36'),
+  ('8','25'),
+  ('3','28'),
+  ('3','42'),
+  ('5','15'),
+  ('4','9'),
+  ('9','5'),
+  ('10','12'),
+  ('7','45');
+INSERT INTO instructor_is_avaliable_time (instructor_id,is_avaliable_time_id)
+VALUES
+  ('3','47'),
+  ('10','35'),
+  ('1','50'),
+  ('7','6'),
+  ('10','11'),
+  ('3','33'),
+  ('8','28'),
+  ('4','40'),
+  ('8','48'),
+  ('6','33');
+INSERT INTO instructor_is_avaliable_time (instructor_id,is_avaliable_time_id)
+VALUES
+  ('1','32'),
+  ('6','36'),
+  ('5','50'),
+  ('7','26'),
+  ('7','15'),
+  ('10','46'),
+  ('4','47'),
+  ('3','33'),
+  ('10','39'),
+  ('7','5');
+INSERT INTO instructor_is_avaliable_time (instructor_id,is_avaliable_time_id)
+VALUES
+  ('5','37'),
+  ('2','36'),
+  ('9','34'),
+  ('8','45'),
+  ('10','26'),
+  ('1','33'),
+  ('6','40'),
+  ('8','34'),
+  ('3','17'),
+  ('10','24');
+INSERT INTO instructor_is_avaliable_time (instructor_id,is_avaliable_time_id)
+VALUES
+  ('5','4'),
+  ('10','21'),
+  ('2','10'),
+  ('6','20'),
+  ('10','18'),
+  ('3','21'),
+  ('3','43'),
+  ('8','23'),
+  ('7','24'),
+  ('8','4');
+INSERT INTO instructor_is_avaliable_time (instructor_id,is_avaliable_time_id)
+VALUES
+  ('9','34'),
+  ('2','46'),
+  ('5','26'),
+  ('1','10'),
+  ('2','28'),
+  ('2','42'),
+  ('7','49'),
+  ('5','42'),
+  ('1','29'),
+  ('8','33');
+INSERT INTO instructor_is_avaliable_time (instructor_id,is_avaliable_time_id)
+VALUES
+  ('10','7'),
+  ('5','12'),
+  ('9','13'),
+  ('1','2'),
+  ('5','41'),
+  ('10','15'),
+  ('4','23'),
+  ('7','34'),
+  ('10','28'),
+  ('2','48');
+INSERT INTO instructor_is_avaliable_time (instructor_id,is_avaliable_time_id)
+VALUES
+  ('9','19'),
+  ('1','46'),
+  ('1','9'),
+  ('4','20'),
+  ('6','22'),
+  ('10','18'),
+  ('6','6'),
+  ('2','50'),
+  ('3','23'),
+  ('3','11');
+
+INSERT INTO student_lesson (lesson_id,student_id)
+VALUES
+  (1,'8, 25, 11, 9, 17, 5, 24'),
+  (2,'3, 9, 6, 20, 13, 24, 12, 14, 8, 23'),
+  (3,'15, 10, 2, 8, 24'),
+  (4,'24'),
+  (5,'6'),
+  (6,'1, 23, 16, 15, 6'),
+  (7,'7, 23, 1, 10, 13, 18, 11, 21, 16, 14'),
+  (8,'2, 22, 10, 4, 15'),
+  (9,'1'),
+  (10,'9, 12');
+INSERT INTO student_lesson (lesson_id,student_id)
+VALUES
+  (11,'16, 18, 24'),
+  (12,'10, 3, 7, 4, 24, 14, 8, 1, 13'),
+  (13,'1, 14, 21, 17'),
+  (14,'18, 10, 16, 12, 9, 25, 13, 1, 19'),
+  (15,'1, 10, 22, 6, 19, 2, 14, 11'),
+  (16,'9, 11, 20'),
+  (17,'8, 19, 5, 22'),
+  (18,'11, 5, 25, 23, 13, 1, 6'),
+  (19,'14, 1'),
+  (20,'6, 11, 3, 22');
+INSERT INTO student_lesson (lesson_id,student_id)
+VALUES
+  (21,'23, 5, 2, 7, 14, 22, 18, 15'),
+  (22,'13'),
+  (23,'23, 22, 24, 10, 12, 2'),
+  (24,'18, 8, 21, 14, 6, 20'),
+  (25,'9, 22, 23, 5, 15, 21, 12, 25, 1'),
+  (26,'15, 21, 1, 14, 10'),
+  (27,'14, 25, 5, 20, 11, 6, 1, 21, 9, 12'),
+  (28,'4, 23, 14, 18, 15, 21, 19, 9'),
+  (29,'13, 25, 20'),
+  (30,'16, 2, 13, 20, 1');
+INSERT INTO student_lesson (lesson_id,student_id)
+VALUES
+  (31,'3, 1, 14, 16, 10, 8, 5, 7'),
+  (32,'23'),
+  (33,'5, 17, 6, 23'),
+  (34,'3, 14, 9, 7, 24'),
+  (35,'4, 9, 12, 10, 14, 21, 7, 1'),
+  (36,'1, 24, 13, 20, 9'),
+  (37,'18, 14, 20, 25, 1, 21, 11, 15, 2, 19'),
+  (38,'21, 6, 20, 8, 5, 3, 15'),
+  (39,'22, 25, 7, 4, 8, 12, 17, 19'),
+  (40,'15, 25, 2');
+
+
+INSERT INTO individual_lesson (lesson_id,instrument_type)
+VALUES
+  ('20','veena','2021-12-05 07:35:46'),
+  ('14','veena','2021-11-29 00:01:10'),
+  ('2','viola','2021-12-15 12:58:24'),
+  ('36','sitar','2021-12-17 02:42:26'),
+  ('18','cello','2021-12-03 22:18:21'),
+  ('30','piano','2021-12-08 20:41:27'),
+  ('23','piano','2021-12-05 18:45:19'),
+  ('35','harp','2021-12-19 22:06:46'),
+  ('10','guitar','2021-12-20 14:12:41'),
+  ('15','sitar','2021-12-03 23:34:08');
+
+INSERT INTO group_lesson (lesson_id,max_number_of_students,min_number_of_students,instrument_type,booked_time)
+VALUES
+  ('17',18,4,'guitar','2021-12-23 19:23:28'),
+  ('3',15,3,'piano','2021-12-06 16:52:27'),
+  ('16',18,5,'mandolin','2021-12-13 18:15:12'),
+  ('40',18,4,'mandolin','2021-12-16 16:17:34'),
+  ('34',16,5,'violin','2021-11-28 08:30:55'),
+  ('4',15,5,'harp','2021-12-22 20:40:35'),
+  ('1',17,5,'viola','2021-12-14 18:00:08'),
+  ('26',17,5,'mandolin','2021-12-07 01:22:25'),
+  ('12',16,5,'mandolin','2021-12-23 23:22:04'),
+  ('8',16,3,'sitar','2021-11-27 09:47:08');
+INSERT INTO group_lesson (lesson_id,max_number_of_students,min_number_of_students,instrument_type,booked_time)
+VALUES
+  ('19',16,4,'cello','2021-12-11 06:31:53'),
+  ('29',17,3,'cello','2021-11-27 14:18:17'),
+  ('33',18,4,'mandolin','2021-12-12 21:17:11'),
+  ('5',16,4,'guitar','2021-12-13 22:04:58'),
+  ('21',20,4,'veena','2021-12-17 01:07:30');
+
+INSERT INTO ensembles (lesson_id,max_number_of_students,min_number_of_students,genre,booked_time)
+VALUES
+  ('6',16,4,'blues','2021-12-05 15:02:21'),
+  ('22',19,4,'rock','2021-12-04 09:09:56'),
+  ('27',20,3,'jazz','2021-12-19 03:03:51'),
+  ('24',19,4,'blues','2021-12-22 23:17:04'),
+  ('32',17,4,'country','2021-11-26 13:37:37'),
+  ('7',20,4,'pop','2021-12-17 05:20:37'),
+  ('28',20,4,'country','2021-12-22 08:36:32'),
+  ('13',18,4,'hip-hop','2021-12-23 10:04:57'),
+  ('9',16,3,'rock','2021-12-22 12:34:26'),
+  ('39',16,4,'rock','2021-12-03 07:15:26');
+INSERT INTO ensembles (lesson_id,max_number_of_students,min_number_of_students,genre,booked_time)
+VALUES
+  ('37',20,3,'country','2021-12-15 01:56:42'),
+  ('31',17,4,'hip-hop','2021-12-03 09:10:51'),
+  ('38',20,4,'soul','2021-11-26 01:41:51'),
+  ('25',19,3,'blues','2021-12-06 11:50:37'),
+  ('11',20,4,'hip-hop','2021-12-18 15:22:50');
+
